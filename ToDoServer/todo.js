f8b7e9948147e0c80e79cf318a36d9d41fa6a206
Yousef Elsayed <yousef4511@outlook.com>
2024-02-05 13:36:01 +0200

    ToDoServer: Apply Eslint Rules

--- ToDoServer/todo.js
+++ ToDoServer/todo.js
@@ -1,42 +1,45 @@
-const { addTodo, listTodos, editToDo, deleteTodo } = require('./todoList');
 const commander = require('commander');
+const {
+  addTodo, listTodos, editToDo, deleteTodo,
+} = require('./todoList');
+
 const program = new commander.Command();
 
-function parseArgAsString(value, prvValue) {
-    let trimmedValue = value.trim();
-    if (!trimmedValue) {
-        throw new commander.InvalidArgumentError('Todo Cannot be empty');
-    }
+function parseArgAsString(value) {
+  const trimmedValue = value.trim();
+  if (!trimmedValue) {
+    throw new commander.InvalidArgumentError('Todo Cannot be empty');
+  }
 
-    return trimmedValue;
+  return trimmedValue;
 }
 
-function parseArgAsInt(arg, prvArg) {
-    const parsedArg = parseInt(arg);
-    if (isNaN(parsedArg)) {
-        throw new commander.InvalidArgumentError('Not a number.');
-    }
-    return parsedArg;
+function parseArgAsInt(arg) {
+  const parsedArg = parseInt(arg, 10);
+  if (Number.isNaN(parsedArg)) {
+    throw new commander.InvalidArgumentError('Not a number.');
+  }
+  return parsedArg;
 }
 
 program.command('add')
-    .description("Add ToDo item")
-    .argument("<string>", "Todo text to add", parseArgAsString)
-    .action(addTodo);
+  .description('Add ToDo item')
+  .argument('<string>', 'Todo text to add', parseArgAsString)
+  .action(addTodo);
 
 program.command('list')
-    .description("List ToDo list")
-    .action(listTodos);
+  .description('List ToDo list')
+  .action(listTodos);
 
 program.command('edit')
-    .description("Edit a ToDo")
-    .argument('<id>', 'ID of ToDo to edit', parseArgAsInt)
-    .argument('<string>', 'Updated ToDo text', parseArgAsString)
-    .action(editToDo);
+  .description('Edit a ToDo')
+  .argument('<id>', 'ID of ToDo to edit', parseArgAsInt)
+  .argument('<string>', 'Updated ToDo text', parseArgAsString)
+  .action(editToDo);
 
 program.command('delete')
-    .description("Delete an existing ToDo")
-    .argument('<id>', 'ID of ToDo to delete', parseArgAsInt)
-    .action(deleteTodo);
+  .description('Delete an existing ToDo')
+  .argument('<id>', 'ID of ToDo to delete', parseArgAsInt)
+  .action(deleteTodo);
 
 program.parse();
--- ToDoServer/todoList.js
+++ ToDoServer/todoList.js
@@ -1,70 +1,73 @@
 const fs = require('fs');
 const path = require('path');
+
 const todosFileName = 'todos.json';
 
 function Todo(id, text) {
-    return {
-        id,
-        text
-    }
-}
-
-function addTodo(text) {
-    let todos = getTodos();
-    todos.push(new Todo(generateId(), text));
-    writeTodosToFile(todos);
-}
-
-function listTodos() {
-    getTodos().forEach((todo) => {
-        console.log(`[${todo.id}] ${todo.text}`);
-    })
-}
-
-function editToDo(id, newTodoText) {
-    let todos = getTodos();
-    let foundTodo = todos.find((todo) => todo.id === id);
-
-    if (!foundTodo) {
-        console.log(`No todo with id [${id}] exists`);
-        return;
-    }
-
-    foundTodo.text = newTodoText;
-    writeTodosToFile(todos);
-}
-
-function deleteTodo(id) {
-    let todos = getTodos();
-
-    let foundTodo = todos.find((todo) => todo.id === id)
-    if (!foundTodo) {
-        console.log(`Todo with id ${id} doesn't exist`);
-        return;
-    }
-    todos = todos.filter((todo) => todo.id !== id);
-    writeTodosToFile(todos);
-}
-
-function generateId() {
-    const todos = getTodos();
-    if (todos.length < 1) {
-        return 1;
-    }
-    return todos[todos.length - 1].id + 1;
+  return {
+    id,
+    text,
+  };
 }
 
 function getTodos() {
-    const isTodosExist = fs.existsSync(path.join(__dirname, todosFileName));
-    if (!isTodosExist) {
-        fs.writeFileSync(todosFileName, '[]');
-    }
+  const isTodosExist = fs.existsSync(path.join(__dirname, todosFileName));
+  if (!isTodosExist) {
+    fs.writeFileSync(todosFileName, '[]');
+  }
 
-    return JSON.parse(fs.readFileSync(path.join(__dirname, todosFileName), 'utf-8'));
+  return JSON.parse(fs.readFileSync(path.join(__dirname, todosFileName), 'utf-8'));
+}
+
+function generateId() {
+  const todos = getTodos();
+  if (todos.length < 1) {
+    return 1;
+  }
+  return todos[todos.length - 1].id + 1;
 }
 
 function writeTodosToFile(newTodos) {
-    fs.writeFileSync(todosFileName, JSON.stringify(newTodos));
+  fs.writeFileSync(todosFileName, JSON.stringify(newTodos));
 }
 
-module.exports = { addTodo, listTodos, editToDo, deleteTodo };
+function addTodo(text) {
+  const todos = getTodos();
+  todos.push(new Todo(generateId(), text));
+  writeTodosToFile(todos);
+}
+
+function listTodos() {
+  getTodos().forEach((todo) => {
+    console.log(`[${todo.id}] ${todo.text}`);
+  });
+}
+
+function editToDo(id, newTodoText) {
+  const todos = getTodos();
+  const foundTodo = todos.find((todo) => todo.id === id);
+
+  if (!foundTodo) {
+    console.log(`No todo with id [${id}] exists`);
+    return;
+  }
+
+  foundTodo.text = newTodoText;
+  writeTodosToFile(todos);
+}
+
+function deleteTodo(id) {
+  let todos = getTodos();
+
+  const foundTodo = todos.find((todo) => todo.id === id);
+  if (!foundTodo) {
+    console.log(`Todo with id ${id} doesn't exist`);
+    return;
+  }
+  todos = todos.filter((todo) => todo.id !== id);
+  writeTodosToFile(todos);
+}
+
+module.exports = {
+  addTodo, listTodos, editToDo, deleteTodo,
+};
